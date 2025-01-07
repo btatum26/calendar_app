b4d0376bfd6ce5ee7fee8cd7e125c4cca60f3d7c
btatum26 <btatum26@vt.edu>
2025-01-06 21:11:41 -0500

    updated express port

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const canvas = require('./routes/canvasAPI.js')
 
+const port = process.env.PORT || 4000;
+
 // Set the web server
 const app = express();
 app.use(express.json());
@@ -10,5 +12,7 @@ app.use(cors());
 app.get('/', (req, res) => res.send('<h1>MERN Example 2: Server</h1>')); // Home web page
 
 app.use('/api', canvas)
+
+app.listen(port, () => {console.log("listening on port: " + port)})
 
 module.exports = app;
